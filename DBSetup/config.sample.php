942399bc93c3a089f56df9a19d61eb2891a6ee0d
RJA36 <rja36@njit.edu>
2020-02-05 13:03:06 -0500

    Update config.sample.php

--- DBSetup/config.sample.php
+++ DBSetup/config.sample.php
@@ -1,11 +1,6 @@
 <?php
-
 $dbuser = "rja36";//your ucid
-
 $dbpass = "password1234";//your phpMyAdmin password
-
 $dbhost = "sql1.njit.edu";//whichever server you login to from web.njit.edu/mysql/phpMyAdmin
-
 $dbdatabase = "rja36";//your ucid
-
 ?>
