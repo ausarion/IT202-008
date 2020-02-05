60c45849660b6df71c89c02cf96f5b3307a327dd
RJA36 <rja36@njit.edu>
2020-02-05 13:08:13 -0500

    Update get_user.php

--- RegistrationPractice/get_user.php
+++ RegistrationPractice/get_user.php
@@ -1,26 +1,14 @@
 <?php
-
 //this is check_db.php
-
 ini_set('display_errors', 1);
-
 ini_set('display_startup_errors', 1);
-
 error_reporting(E_ALL);
 
-
-
 require("config.php");
-
 echo "DBUser: " .  $dbuser;
-
 echo "\n\r";
 
-
-
 $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-
-
 
 try{
 
@@ -28,24 +16,14 @@ try{
 
 	echo "Should have connected";
 
-	
 
-
-
-	 $stmt = $db->prepare("SELECT * from `Users2` where id = :id");
-
-    
-
+	 $stmt = $db->prepare("SELECT * from `Users2` where id = :id");   
         $params = array(":id"=> '1');
-
         $stmt->execute($params);
-
 	$results = $stmt->fetch(PDO::FETCH_ASSOC);
 
         echo "<pre>" . var_export(
-
                         $stmt->errorInfo(), true) . "</pre>";
-
 	echo var_export($results, true);
 
 }
@@ -53,12 +31,8 @@ try{
 catch(Exception $e){
 
 	echo $e->getMessage();
-
 	exit("It didn't work");
-
 }
-
-
 
 ?>
 
