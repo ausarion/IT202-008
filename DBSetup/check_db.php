76b47369077664474d9d02db3fcc2e09d16b9d53
Rakkim Abdul-Shakoor <rja36@njit.edu>
2020-02-05 12:03:40 -0500

    file edited

---
+++ DBSetup/check_db.php
@@ -0,0 +1,58 @@
+
+
+<?php
+// this is to check_db_php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require("config.php");
+echo "DBUser: " . $dbuser;
+echo "\n\r";
+
+$connection_string = "mysql:host=$dbhost;dbname=$database;charset=utf8mb4;
+
+try{
+
+        $db = new PDO($connection_string, $dbuser, $dbpass);
+
+	echo "Should have connected";
+
+	
+
+
+
+	 $stmt = $db->prepare("INSERT INTO `Users2`
+
+                        (email) VALUES
+
+                        (:email)");
+
+    
+
+        $params = array(":email"=> 'Bob@bob.com');
+
+        $stmt->execute($params);
+
+        echo "<pre>" . var_export(
+
+                        $stmt->errorInfo(), true) . "</pre>";
+
+	echo var_export($stmt->errorInfo(), true);
+
+}
+
+catch(Exception $e){
+
+	echo $e->getMessage();
+
+	exit("It didn't work");
+
+}
+
+
+
+?>
+?>
+
+
---
+++ config.php
@@ -0,0 +1,5 @@
+<?
+<$dbuser = "rja36";
+ $dbpass = "Operation@2020";
+ $db database = "rja36';
+?>
