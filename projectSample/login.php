f057edddf4b47a13b4ced49974127de3f46fe4df
RJA36 <rja36@njit.edu>
2020-02-23 18:24:16 -0500

    made Some changes

--- projectSample/login.php
+++ projectSample/login.php
@@ -31,7 +31,7 @@ if(isset($_POST['email']) && isset($_POST['password']) && !empty($_POST['passwor
 	$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 	try {
 		$db = new PDO($connection_string, $dbuser, $dbpass);
-		$stmt = $db->prepare("SELECT id, email, password from `Users3` where email = :email LIMIT 1");
+		$stmt = $db->prepare("SELECT id, email, password from `TestUsers` where email = :email LIMIT 1");
 
         $params = array(":email"=> $email);
         $stmt->execute($params);
