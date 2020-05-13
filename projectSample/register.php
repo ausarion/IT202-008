9aee1194e31673e6de5a9bed997e1299fc2f972f
RJA36 <rja36@njit.edu>
2020-05-13 12:23:52 -0400

    deleted extra code

--- projectSample/register.php
+++ projectSample/register.php
@@ -250,55 +250,6 @@ if (   isset($_POST['FirstName'])
  </body>
 </html>
 
-<?php 
-ini_set('display_errors',1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-if(	   isset($_POST['email']) 
-	     
-       
-        && isset($_POST['password'])
-	      && isset($_POST['confirm'])
-	      ){
-	      $pass = $_POST['password'];
-	      $conf = $_POST['confirm'];
-	      if($pass == $conf){
-		            echo "All good, 'registering user'";
-      	}
-	      else{
-	      echo "What's wrong with you? Learn to type";
-	      exit();
-      	}
-      	//let's hash it
-	      $pass = password_hash($pass, PASSWORD_BCRYPT);
-	
-        echo "<br>$pass<br>";
-	      //it's hashed
-	      require("config.php");
-	
-        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 
-	      try {
-	        	$db = new PDO($connection_string, $dbuser, $dbpass);
-	                $stmt = $db->prepare "INSERT INTO `TestUsers`(email, password) VALUES (:email, :password)";
-		       
-		        $email = $_POST['email'];
-                        $password = $_POST['password'];
-                        $params = array(:email"=> $email, 
-                                        ":password"=> $pass);
-                        $stmt->prepare($params);
-        
-
-                       echo "<pre>" . var_export($stmt->errorInfo(), true) . "</pre>";
-        }
-        
-	      catch(Exception $e){
-		            echo $e->getMessage();
-    		        exit();
-
-	      } 
-}
-             
-?>
  
 >>>>>>> origin/master
